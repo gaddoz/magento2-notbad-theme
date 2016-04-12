d39222fdb11e912339b06ee0b84008918eb7d7d9
Serhii Borodai <serhii.borodai@symmetrics.de>
2016-04-12 10:26:03 +0300

    init commit

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright © 2015 Bratez. All rights reserved.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/Bratez/Notbad',
+    __DIR__
+);
